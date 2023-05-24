bfe82024d614aaa69b547dc2cfcf2d04a977b33d
Collins Kiprono <101986591+iamkiprono@users.noreply.github.com>
2023-05-24 13:10:11 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div>
       <nav className="flex p-6 max-w-7xl m-auto justify-between items-center ">
        <img className="w-32" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
-        <button className="rounded px-4  py-1 bg-reddy text-white">Sign In</button>
+        <button className="rounded px-4  py-1 bg-reddy text-white">Sign In to get Updates</button>
       </nav>
     </div>
   );
